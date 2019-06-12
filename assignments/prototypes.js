0bcc683a3533d82b0cccc2e6ea1770aa33f4febe
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-06-12 21:36:56 +0100

    WIP: prototypes.js

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -16,12 +16,29 @@
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
 
+function GameObject(createdAt, name, dimensions) {
+  this.createdAt = createdAt,
+  this.name = name,
+  this.dimensions = dimensions
+}
+GameObject.prototype.destroy = function() {
+  return `${this.name} was removed from the game.`;
+}
+
 /*
   === CharacterStats ===
   * healthPoints
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+
+function CharacterStats(healthPoints) {
+  this.healthPoints = healthPoints
+}
+CharacterStats.prototype = Object.create(GameObject.prototype);
+CharacterStats.prototype.takeDamage = function() {
+  return `${this.name} took damage.`;
+}
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -33,6 +50,16 @@
   * should inherit takeDamage() from CharacterStats
 */
  
+function Humanoid(team, weapons, language) {
+  this.team = team,
+  this.weapons = weapons,
+  this.language = language
+}
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+Humanoid.prototype.greet = function () {
+  return `${this.name} offers a greeting in ${this.language}.`
+}
+
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
